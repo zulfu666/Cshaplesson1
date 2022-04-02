39b92f24db1f1b4fced14a5598cf14dbf3f289c7
ZulfuAbiyev <74251393+zulfu666@users.noreply.github.com>
2022-04-02 16:19:51 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int a = 9;
+decimal b = 4.5M;
+double d = 5.6;
+string c = "Salam";
+bool e = false;
+var f = "type";
+var cem = a + b;
+
+//int[] myArr = { 1, 2, 3 };
+//string[] users = { "Zulfu", "Ruslan", "Ayxan", "Sekine", "Tofiq" };
+
+//for (int i = 0; i < users.Length; i++)
+//{
+//    Console.WriteLine(users[i]
+//        ); 
+//}
+
+
+//int number = 0;
+
+//while (number < 9)
+//{
+//    Console.WriteLine(number);
+//    number++;
+//}
+
+
+//bool isDelete = false;
+
+
+//if (isDelete) {
+//{
+//        Console.WriteLine("Silinmis mehsullar.");
+//}
+//    else {
+
+
+Console.WriteLine   
